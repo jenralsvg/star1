208b32d9d4aa20c217cb848e7007c6663123902f
KilL4Life <76258827+jenralsvg@users.noreply.github.com>
2021-02-22 23:12:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -73,10 +73,7 @@ const welkom = JSON.parse(fs.readFileSync('./database/group/welkom.json'))
 const nsfw = JSON.parse(fs.readFileSync('./database/group/nsfw.json'))
 const samih = JSON.parse(fs.readFileSync('./database/group/simi.json'))
 const event = JSON.parse(fs.readFileSync('./database/group/event.json'))
-const _limit = JSON.parse(fs.readFileSync('./database/user/limit.json'))
-const uang = JSON.parse(fs.readFileSync('./database/user/uang.json'))
 const ban = JSON.parse(fs.readFileSync('./database/user/banned.json'))
-const premium = JSON.parse(fs.readFileSync('./database/user/premium.json'))
 const antilink = JSON.parse(fs.readFileSync('./database/group/antilink.json'))
 /*       
 ]=====> LOAD MENU <=====[
