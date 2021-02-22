d9b2ba5afcf2d100d1894783c10f30816f86c4a3
KilL4Life <76258827+jenralsvg@users.noreply.github.com>
2021-02-22 10:25:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1885,9 +1885,8 @@ case 'sound7':
 tujuh = fs.readFileSync('./assets/sound7.mp3');
 enzet.sendMessage(from, tujuh, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
 break				
-if (budy.includes('assalamualaikum'))
-      reply(from, './assets/Salam.opus')
-  enzet.sendMessage(from, reply, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
+if (budy.includes('assalamualaikum'){
+   enzet.sendPtt(from, './assets/Salam.opus',MessageType)
           }
 /*
 ]=====> Thanks TO <=====[
